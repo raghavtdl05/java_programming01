20904004721028a9e76b73ae26745355f702660d
Dell <raghavtdl05@gmail.com>
2022-05-16 14:31:00 +0530

    if and else

---
+++ src/java_conditional_statment/Statement_if.java
@@ -0,0 +1,10 @@
+package java_conditional_statment;
+
+public class Statement_if {
+    public static void main(String[] args) {
+        int roll=10;
+        if (roll > 9);{
+            System.out.println("permitted for exam");
+        }
+    }
+}
---
+++ src/java_conditional_statment/Statement_if_else.java
@@ -0,0 +1,14 @@
+package java_conditional_statment;
+
+public class Statement_if_else {
+    public static void main(String[] args) {
+        int roll=8;
+        if(roll>9){
+            System.out.println("Permitted for exam");
+
+        }
+       else {
+            System.out.println("not permitted for exam");
+        }
+    }
+}
