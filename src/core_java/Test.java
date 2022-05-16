ef056d56de5490431e19928992a729d8cc89ab82
Dell <raghavtdl05@gmail.com>
2022-05-16 12:25:53 +0530

    GIT Class

---
+++ src/core_java/Test.java
@@ -0,0 +1,9 @@
+package core_java;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Exam");
+        System.out.println("Subject");
+        System.out.println("Result");
+    }
+}
