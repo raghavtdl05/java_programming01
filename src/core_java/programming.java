d58a6ae7cb4d81f4a32f6a18f0ac55d9e90ad9ac
Dell <raghavtdl05@gmail.com>
2022-05-16 11:56:38 +0530

    java program

---
+++ src/core_java/programming.java
@@ -0,0 +1,10 @@
+package core_java;
+
+public class programming {
+    public static void main(String[] args) {
+        System.out.println("java class");
+        System.out.println("java learning");
+        System.out.println("Test");
+        System.out.println("paper");
+    }
+}
