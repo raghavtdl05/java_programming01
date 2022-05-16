f6c9c5c24dd9578fb6bae45dfad504fd41d12630
Dell <raghavtdl05@gmail.com>
2022-05-16 13:59:56 +0530

    electrical

---
+++ src/java_variable/Variable_of_java.java
@@ -0,0 +1,16 @@
+package java_variable;
+
+
+
+public class Variable_of_java {
+    public static void main(String[] args) {
+        String student_name ="Raghav";
+        System.out.println("Print student name"+student_name);
+        int age = 23 ;
+        System.out.println("Print age"+age);
+        double salary =5600.500;
+        System.out.println("Print salary"+salary);
+
+    }
+
+}
